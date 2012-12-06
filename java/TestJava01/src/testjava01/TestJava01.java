68034d4796d3b9a7db7714b42f778dbb39389845
keith <keith@abacusindustriesinc.com>
2012-12-06 09:59:42 -0600

    Testing..

--- java/TestJava01/src/testjava01/TestJava01.java
+++ java/TestJava01/src/testjava01/TestJava01.java
@@ -4,13 +4,12 @@
  */
 package testjava01;
 
-/**
- *
+/*
  * @author Gunny
  */
 public class TestJava01 {
 
-    /**
+    /*
      * @param args the command line arguments
      */
     public static void main(String[] args) {
