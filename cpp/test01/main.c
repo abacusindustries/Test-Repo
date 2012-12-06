432cae2f39d229d67832fb24b4c423116d40f842
keith <keith@abacusindustriesinc.com>
2012-12-06 09:53:02 -0600

    Testing..

---
+++ cpp/test01/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  cTest01
+//
+//  Created by Gunny on 9/14/12.
+//  Copyright (c) 2012 Abacus Industries, Inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    printf("Hello, World!\n");
+    
+    int x = 1;
+    
+    while ( x <= 10 )
+    {
+        printf("%d \n",x) ;
+        x++;
+    }
+
+    printf("Goodbye!\n");
+    
+    return 0;
+}
+
